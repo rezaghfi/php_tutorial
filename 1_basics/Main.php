29601ef4643b65a29e00ba340870cc6abb3e0206
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-03-16 12:31:37 +0330

    change basic

---
+++ 1_basics/Main.php
@@ -0,0 +1,12 @@
+<?php
+// data type in php
+# cant comment with // and # in php
+
+$a = 'a';
+$b = 1;
+$c = 1.3434;
+$d = 343434343.34343434;
+
+print('$a = '.$a);
+
+echo ' <br> $b = '.$b;
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__.'/oop/Person.php';
-$person = new Person('reza', 25);
+require_once __DIR__.'/1_basics/Main.php';
+require_once __DIR__.'/3_oop/Person.php';
 
-echo "person name is: ". $person->getName();
+
+$person = new Person('reza', 25);
+echo "<br> person name is: ". $person->getName();
