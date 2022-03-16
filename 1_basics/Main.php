42158386efef3af1b8ed696a61a5092b869ee168
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-03-16 15:54:20 +0330

    change

--- 1_basics/Main.php
+++ 1_basics/Main.php
@@ -2,7 +2,8 @@
 // data type in php
 # cant comment with // and # in php
 
-$a = 'a';
+function variable(){
+  $a = 'a';
 $b = 1;
 $c = 1.3434;
 $d = 343434343.34343434;
@@ -10,3 +11,5 @@ $d = 343434343.34343434;
 print('$a = '.$a);
 
 echo ' <br> $b = '.$b;
+}
+
--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 require_once __DIR__.'/1_basics/Main.php';
 require_once __DIR__.'/3_oop/Person.php';
 
+//variable();
+//phpinfo();
+//exit();
 
-$person = new Person('reza', 25);
-echo "<br> person name is: ". $person->getName();
+$person = new Person('reza');
+echo "<br> person name is: ". $person->getName(); 
