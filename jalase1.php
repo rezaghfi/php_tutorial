d4921e629bb2367d1721925c310014f924b297b8
reza gholamalitabar <reza.gh.fi@gmail.com>
2023-03-08 00:24:08 +0330

    jalase1 commit

---
+++ jalase1.php
@@ -0,0 +1,52 @@
+<?php
+
+// variable
+
+$number1 = 1;
+$number2 = 2;
+
+//$# = = error
+//$1n == error
+
+$x = 1.2;
+$y = 1.5;
+$z = $x + $y;
+
+// output php
+echo $z."\n";
+echo($z."\n");
+print("\t".$z."\n");
+
+
+// variable  $ncsd21212_
+// data type
+
+// integer float character string boolean array object null resource
+
+// operator
+// عملگرهای ریاضیاتی
+// + - * / % ++ --
+// integer
+$x = 12;
+$y = 11;
+$z = 1;
+// casting integer -> float
+//float
+$z = $x / $y;
+print($y - $x . "\n");
+
+print ($z. "\n");
+
+print($x++ . "\n");
+print($x . "\n");
+print(++$x . "\n");
+
+print($x % $y."\n");
+
+// comment
+# comment
+
+$x = 1.23;
+print($x."\n");
+
+// عملگر مقایسه ای
---
+++ site.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>dfdfdfdf</h1>
+<?php
+// This is a single-line comment
+echo "reza";
+$text = 1;
+echo "<br>".$text;
+# This is also a single-line comment
+?>
+
+</body>
+</html>
